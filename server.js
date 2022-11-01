d26fa8bf3bb066107aeadb1235ed462a721e2f54
ruben <ruben.eduardo.tec@gmail.com>
2022-10-31 22:46:58 -0500

    fix server.js issue

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const app = express();
 
-app.use(express.static(__dirname + 'dist'));
-app.all('*', (req, res)=> {
-    res.status(200).sendFile(__dirname + '/dist/index.html')
+app.use(express.static('./dist/'));
+
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/' });
 });
 
 app.listen(process.env.PORT || 8080);
