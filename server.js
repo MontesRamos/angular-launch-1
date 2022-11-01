66bafb40fc27f431cb77c6603ebf73f6ff2bd56e
ruben <ruben.eduardo.tec@gmail.com>
2022-10-31 22:50:46 -0500

    fix server.js issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static('./dist/'));
 
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
+  res.sendFile('index.html', { root: 'dist/angular-launch-1/' });
 });
 
 app.listen(process.env.PORT || 8080);
